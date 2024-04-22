dee6e5e46cfca135127ab1d68b953a310e468279
Sarah Welcher <sarahwelcher@hotmail.co.uk>
2024-04-22 19:37:52 +0000

    Add modal to game rules button, style with CSS and add interactivity with JavaScript

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+// Homepage modal - W3 Schools tutorial used
+function openModal() {
+    document.getElementById('game-rules').style.display='block';
+  }
+  
+  function closeModal() {
+  document.getElementById('game-rules').style.display='none';
+  }
