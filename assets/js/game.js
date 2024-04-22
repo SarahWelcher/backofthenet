dbed52d14c5214cb3e010fd33fae887b84b5c822
Sarah Welcher <sarahwelcher@hotmail.co.uk>
2024-04-22 20:39:07 +0000

    Add variables for questions, answers and score to game.js

---
+++ assets/js/game.js
@@ -0,0 +1,8 @@
+//  Declare questions and answers
+const questionElement = document.getElementById("question");
+const optionsElement = document.getElementById("options");
+
+// Declare current question and score area
+let currentQuestion = 0;
+let score = 0;
+
